34baed79e32049f168cbb312630484accda562b4
Olivia Cunningham <Liv.cunninghamgb@gmail.com>
2019-05-28 17:16:13 -0400

    blk jack deals on button click

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import Header from './components/Header'
 
 class App extends Component {
+  state = {
+    game: {
+      cards: []
+    }
+  }
+
+  //fetch new deck
+  letsPlay() {
+    fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=2')
+      .then(resp => resp.json())
+      .then(newDeck => {
+        console.log(newDeck)
+        this.setState({
+          game: newDeck
+        })
+      })
+  }
+
+  //show dealt cards
+  //create dealer hand
+  //create player hand
+  //create hit, stay, play buttons
+
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <Header />
+        <main>
+          <div className="start-game">
+            {this.state.game.cards.map((card, i) => {
+              return <img src={card.image} />
+            })}
+            <button className="lets-play" onClick={() => this.letsPlay()} />
+          </div>
+        </main>
+      </>
+    )
   }
 }
 
